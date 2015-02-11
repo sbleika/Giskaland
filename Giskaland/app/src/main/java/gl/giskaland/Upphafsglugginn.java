7ae00caf43739424b7a9f81e90a5a5524568c645
tumsgis <tumsgis@gmail.com>
2015-02-11 11:48:53 +0000

    Grunnútgáfa af tökkum búin til

--- Giskaland/app/src/main/java/gl/giskaland/Upphafsglugginn.java
+++ Giskaland/app/src/main/java/gl/giskaland/Upphafsglugginn.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
 
 
 public class Upphafsglugginn extends ActionBarActivity {
