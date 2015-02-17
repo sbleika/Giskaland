db07d0b8c8e3790136d07abb2dab9e90b9d6e045
tumsgis <tumsgis@gmail.com>
2015-02-17 13:39:28 +0000

    Tómt GameActivity

--- Giskaland/app/src/main/java/gl/giskaland/SecondLevel.java
+++ Giskaland/app/src/main/java/gl/giskaland/SecondLevel.java
@@ -1,9 +1,11 @@
 package gl.giskaland;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class SecondLevel extends ActionBarActivity {
@@ -12,6 +14,11 @@ public class SecondLevel extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_level);
+    }
+
+    public void openGameActivity (View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 
 
