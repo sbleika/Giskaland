41f7f058b5eec7d3d0680b1b0f3ec600be0b1537
Bourinator <egillorn89@gmail.com>
2015-04-12 15:05:34 +0000

    Sma fikt i popup

--- Giskaland/app/src/main/java/gl/giskaland/DbManager.java
+++ Giskaland/app/src/main/java/gl/giskaland/DbManager.java
@@ -29,7 +29,7 @@ public class DbManager extends SQLiteOpenHelper {
     private SQLiteDatabase myDb;
     private final Context myContext;
 
-    private static final int DB_VERSION = 11;
+    private static final int DB_VERSION = 25;
 
     /**
      *  Constructor for the DbManager.
